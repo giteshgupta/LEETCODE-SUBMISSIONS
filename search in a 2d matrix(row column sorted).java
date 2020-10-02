e544b45a2e8c7d51c41186676ca804ca1fd685d0
Gitesh Gupta <64272242+giteshgupta@users.noreply.github.com>
2020-10-02 23:27:31 +0530

    search in a 2d array(row colum sorted)

---
+++ search in a 2d matrix(row column sorted).java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       int r=matrix.length;
+        if(r==0)
+        {
+            return false;
+        }
+        int c=matrix[0].length;
+        int i=0, j=c-1;
+        while(i<r && j>=0)
+        {
+            if(matrix[i][j]==target)
+            {
+                return true;
+            }
+            else if(matrix[i][j]>target)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return false;
+        
+    }
+}
