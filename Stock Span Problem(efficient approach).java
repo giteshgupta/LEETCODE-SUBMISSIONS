83ba80b651222f9ca401ce7963331a258d8e51b6
Gitesh Gupta <64272242+giteshgupta@users.noreply.github.com>
2020-10-07 23:07:34 +0530

    Stock Span Problem

---
+++ Stock Span Problem(efficient approach).java
@@ -0,0 +1,19 @@
+class StockSpanner {
+
+    Stack <int[]> st;
+    public StockSpanner()
+    {
+        st=new Stack<>();
+    }
+    public int next(int price) 
+    {
+        int span=1;
+        while(!st.isEmpty() && price>=st.peek()[0])
+        {
+            span+= st.peek()[1];
+            st.pop();
+        }  
+        st.push(new int[]{price, span});// every time new instance will be created.
+        return span;
+    }
+}
