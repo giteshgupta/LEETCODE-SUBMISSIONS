5b686dc749a318f325eeca5ab855022417c6bf53
Gitesh Gupta <64272242+giteshgupta@users.noreply.github.com>
2020-10-13 23:32:09 +0530

    Check If Binary Search Tree(BST) or not

--- to check if given tree is BST.java
+++ to check if given tree is BST.java
@@ -1,11 +1,14 @@
 class Solution {
     int prev= Integer.MIN_VALUE;
     public boolean isValidBST(TreeNode root) {
-       if(root.val<0) return false;
+       
         if(root==null) return true;
+
         if(isValidBST(root.left)==false) return false;
+
         if(prev>=root.val) return false;
             prev=root.val;
+
          return isValidBST(root.right);
         
     }
